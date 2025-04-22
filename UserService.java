a8971d054df461a3d4ed495262da4df7de77a6a9
vaibhavraj-exe <vraj.exe@gmail.com>
2025-04-22 12:59:13 +0530

    chore: add UserService.java

---
+++ UserService.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private List<String> users = new ArrayList<>();
+
+    public void addUser(String user) {
+        users.add(user);
+        System.out.println("User added: " + user);
+    }
+
+    // this is to create and resolve a conflict
+
+    public void removeUser(String user) {
+        if (users.remove(user)) {
+            System.out.println("User removed: " + user);
+        } else {
+            System.out.println("User not found: " + user);
+        }
+    }
+
+    public List<String> getAllUsers() {
+        return new ArrayList<>(users);
+    }
+}
